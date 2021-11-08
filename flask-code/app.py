9089993180178fa346b0dfe1bbfcbb0d2adbc14e
fabiolarobles1 <fabiola.robles1@upr.edu>
2021-11-08 13:32:40 -0400

    some nits

--- flask-code/app.py
+++ flask-code/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from controller.users import BaseUsers
 from controller.userrole import BaseUserRole
-#from controller.room import BaseRoom
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -12,14 +11,16 @@ CORS(app)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
 @app.route('/users', methods=['GET', 'POST'])
 def handle_users():
     if request.method == 'GET':
-         return BaseUsers().getAllUsers()
+        return BaseUsers().getAllUsers()
     elif request.method == 'POST':
-         return BaseUsers().addNewUser(request.json)
+        return BaseUsers().addNewUser(request.json)
     else:
         return jsonify("Method Not Allowed."), 405
+
 
 @app.route('/userrole', methods=['POST'])
 def handle_userrole():
@@ -40,16 +41,6 @@ def handle_usersid(userid):
 
     else:
         return jsonify("Method Not Allowed."), 405
-
-# @app.route('/room', methods=['POST', 'GET'])
-# def handle_room():
-#     if request.method == 'POST':
-#         return BaseRoom().addNewRoom(request.json)
-#     if request.method = 'GET':
-#         return BaseRoom().getAllRooms()
-
-
-#@app.route('/room/<int: ')
 
 
 if __name__ == '__main__':
