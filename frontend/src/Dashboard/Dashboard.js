323b9cf8376a3105bd104ec13429b5233b16a344
fabiolarobles1 <fabiola.robles1@upr.edu>
2021-12-04 09:56:48 -0400

    added top menu

--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,30 +1,35 @@
-import React, {Component, useState} from 'react';
-import {Calendar, momentLocalizer, Views } from 'react-big-calendar';
+import React, { Component, useState } from 'react';
+import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import {Button, Card, Container, Modal} from "semantic-ui-react";
-import {Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis} from "recharts";
+import { Button, Card, Container, Modal, Segment } from "semantic-ui-react";
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from "recharts";
+import TopBarMenu from '../Menus/TopBarMenu';
 
 
-function BookMeeting(){
-    const [data, setData] = useState([{"name": 1, "Counts": 5},
-                                                {"name": 2, "Counts": 4},
-                                                {"name": 3, "Counts": 3},
-                                                {"name": 4, "Counts": 2},
-                                                {"name": 5, "Counts": 1}]);
+function BookMeeting() {
+    const [data, setData] = useState([{ "name": 1, "Counts": 5 },
+    { "name": 2, "Counts": 4 },
+    { "name": 3, "Counts": 3 },
+    { "name": 4, "Counts": 2 },
+    { "name": 5, "Counts": 1 }]);
 
-    return <Container style={{ height: 800 }}>
+    return (
+        <Segment>
+            <TopBarMenu active="Dashboard" />
+            <Container style={{ height: '100%' }}>
 
-        <BarChart width={730} height={250} data={data}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="Counts" fill="#8884d8" />
-        </BarChart>
-    </Container>
-
+                <BarChart width={730} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="Counts" fill="#8884d8" />
+                </BarChart>
+            </Container>
+        </Segment>
+    )
 
 }
 export default BookMeeting;
